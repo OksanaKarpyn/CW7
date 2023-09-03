713d21d138dc53bd46e36f5ea150a472a23a0531
Oksana Karpyn <oksana101992@gmail.com>
2023-09-03 21:46:26 +0200

    add downland img

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ let changeImgButton = document.querySelector('.change-img');
 let changeTextButton = document.querySelector('.change-text');
 let foto = document.querySelector('.foto');
 let text = document.querySelector('.text');
+let input = document.querySelector('#input');
+let title = document.querySelector('.preview-title');
+let downImgButton = document.querySelector('.down-img');
+// elemento che voglio dopo trasformare in img
+let perview = document.querySelector('.preview');
 
 let currentIndex = 1;
 let changeCount = 4;
@@ -18,11 +23,33 @@ function imgChange(){
 }
 function textChange(){
     currentIndex++;
-    if(currentIndex > changeCount){
+    if(currentIndex > changeCount){ 
         currentIndex = 1;
     }
     text.src = `./assets/images/txt_${currentIndex}.png`;
 }
+function titleChange(){
+    let valueInput = input.value;
+    if(valueInput.trim().length > 0){
+
+        title.innerText=`Auguri ${valueInput}`;
+    }
+    else{
+        title.innerHTML = 'Auguri Caro'
+    }
+    console.log(valueInput);
+}
+function downlandImg(){
+    htmlToImage.toJpeg(perview)
+  .then(function (dataUrl) {
+    var link = document.createElement('a');
+    link.download = 'my-image-name.jpeg';
+    link.href = dataUrl;
+    link.click();
+  });
+}
 // evento dimanico
 changeImgButton.addEventListener('click',imgChange);
 changeTextButton .addEventListener('click',textChange);
+input.addEventListener('input',titleChange);
+downImgButton.addEventListener('click',downlandImg);
